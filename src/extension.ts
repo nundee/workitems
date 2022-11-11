6bce9169a21a82de7e543832de8e666ef90a7b59
Adnand.Dragoti <Adnand.Dragoti@avl.com>
2022-11-11 13:25:30 +0100

    abandon cherry picking #20

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,32 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { getGitApiAsync } from "./gitExtension";
 import { WorkItemProvider } from './workItemProvider';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "workitems" is now active!');
-
-    const wiProvider = new WorkItemProvider();
-    wiProvider.registerAll(context);
+    console.info('Congratulations, your extension "workitems" is now active!');
+    const gitApi  = await getGitApiAsync();
+    if(gitApi) {
+        const repos=gitApi.repositories;
+        if(!repos || repos.length<1) {
+            console.info("No repos yet. Register to onDidOpenRepository event");
+            const openRepoDisposable = gitApi.onDidOpenRepository(async e => {
+                await e.status();
+                (new WorkItemProvider()).registerAll(context);
+                openRepoDisposable.dispose();
+            });
+            context.subscriptions.push(openRepoDisposable);
+        } else {
+            (new WorkItemProvider()).registerAll(context);
+        }
+    }
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+    console.info('your extension "workitems" is now inactive!');
+}
